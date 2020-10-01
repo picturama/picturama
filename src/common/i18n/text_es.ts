818cf1895eaf9a1757ebeb973be57691a3cf5f9e
Fer Vittorello <fvittorello@gmail.com>
2020-10-01 04:12:41 -0300

    add missing spanish translations

--- src/common/i18n/text_es.ts
+++ src/common/i18n/text_es.ts
@@ -1,154 +1,159 @@
 export default {
+    common_cancel: "Cancelar",
+    common_backToLibrary: "Volver a la biblioteca",
+    common_ratio: "{0} de {1}",
+    common_error_photoNotExisting: `Foto no encontrada`,
+    common_error_photoNotExisting_desc: `Esta foto ya no se encuentra donde estaba ubicada en el último escaneo. Quizás fue movida, borrada o se encuentra en un disco que no está conectado`,
 
-    common_cancel: 'Cancelar',
-    common_backToLibrary: 'Volver a la biblioteca',
-    common_ratio: '{0} de {1}',
-    common_error_photoNotExisting: ``,  // TODO: 'Photo is no longer there'
-    common_error_photoNotExisting_desc: ``,  // TODO: 'This photo is no longer where it was on the last scan. Maybe it has been moved, deleted or it is on a disk that is no longer connected.'
+    App_error_noWebGL_title: `WebGL deshabilitado`,
+    App_error_noWebGL_desc: `Picturama necesita una tarjeta gráfica con aceleración 3D para poder funcionar. Por favor revisa en la configuración de tu sistema si es posible activar la aceleración 3D y reinicia Picturama.`,
 
-    App_error_noWebGL_title: ``,  // TODO: 'No WebGL'
-    App_error_noWebGL_desc: ``,  // TODO: 'Picturama needs a graphic card with 3D acceleration in order to function. Please have a look in the system settings whether you can activate 3D acceleration and restart Picturama.'
+    CropModeToolbar_aspect_free: "Libre",
+    CropModeToolbar_aspect_original: "Original",
+    CropModeToolbar_aspect_square: "Cuadrado",
+    CropModeToolbar_changeToPortrait: "Cambiar a retrato",
+    CropModeToolbar_changeToLandscape: "Cambiar al modo horizontal",
+    CropModeToolbar_reset: "Reiniciar",
+    CropModeToolbar_done: "Hecho",
 
-    CropModeToolbar_aspect_free: 'Libre',
-    CropModeToolbar_aspect_original: 'Original',
-    CropModeToolbar_aspect_square: 'Cuadrado',
-    CropModeToolbar_changeToPortrait: 'Cambiar a retrato',
-    CropModeToolbar_changeToLandscape: 'Cambiar al modo horizontal',
-    CropModeToolbar_reset: 'Reiniciar',
-    CropModeToolbar_done: 'Hecho',
+    ErrorController_copied: "Informe copiado al portapapeles.",
 
-    ErrorController_copied: 'Informe copiado al portapapeles.',
+    ErrorToast_title: "Oh no! Algo ha ido mal.",
+    ErrorToast_hideReport: "Ocultar informe",
+    ErrorToast_showReport: "Mostrar informe",
+    ErrorToast_copy: "Copiar",
 
-    ErrorToast_title: 'Oh no! Algo ha ido mal.',
-    ErrorToast_hideReport: 'Ocultar informe',
-    ErrorToast_showReport: 'Mostrar informe',
-    ErrorToast_copy: 'Copiar',
+    ExportDialog_title_one: `Exportar foto`,
+    ExportDialog_title_more: `Exportar {0} fotos`,
+    ExportDialog_format: `Formato`,
+    ExportDialog_quality: `Calidad`,
+    ExportDialog_quality_low: `baja ({0}) - menor tamaño`,
+    ExportDialog_quality_medium: `media ({0})`,
+    ExportDialog_quality_high: `alta  ({0})`,
+    ExportDialog_quality_max: `máxima  ({0})`,
+    ExportDialog_size: `Tamaño`,
+    ExportDialog_size_S: `pequeño ({0})`,
+    ExportDialog_size_M: `mediano ({0})`,
+    ExportDialog_size_L: `grande ({0})`,
+    ExportDialog_size_original: `tamaño orignal`,
+    ExportDialog_size_custom: `personalizado`,
+    ExportDialog_customSizeSettings: `Máximo {0} de {1} pixeles`,
+    ExportDialog_customSizeSide_width: `ancho`,
+    ExportDialog_customSizeSide_height: `alto`,
+    ExportDialog_customSizeSide_size: `tamaño`,
+    ExportDialog_privacy: `Privacidad`,
+    ExportDialog_removeInfo: `Quitar información extra`,
+    ExportDialog_removeInfo_desc: `Cuando esta opción esté seleccionada, la información extra como las etiquetas EXIF serán quitadas de la foto exportada. Esta información es acerca de la cámara que fue utilizada, la locación de la foto o similares.`,
+    ExportDialog_fileName: `Nombre del archivo`,
+    ExportDialog_fileName_likeOriginal: `Como el original`,
+    ExportDialog_fileName_sequence: `secuencia`,
+    ExportDialog_fileName_sequencePrefix: `Prefijo`,
+    ExportDialog_fileName_sequencePrefixDefault: `Foto`,
+    ExportDialog_fileName_sequenceExample: `Ejemplo: {0}`,
+    ExportDialog_export: `Exportar`,
+    ExportDialog_exportingTo: `Exportando a {0}`,
+    ExportDialog_done_one: `Se terminó de exportar la foto`,
+    ExportDialog_done_more: `Se terminó de exportar {0} fotos`,
 
-    ExportDialog_title_one: ``,  // TODO: 'Export photo'
-    ExportDialog_title_more: ``,  // TODO: 'Export {0} photos'
-    ExportDialog_format: ``,  // TODO: 'Format'
-    ExportDialog_quality: ``,  // TODO: 'Quality'
-    ExportDialog_quality_low: ``,  // TODO: 'low ({0}) - smallest size'
-    ExportDialog_quality_medium: ``,  // TODO: 'medium ({0})'
-    ExportDialog_quality_high: ``,  // TODO: 'high ({0})'
-    ExportDialog_quality_max: ``,  // TODO: 'maximum ({0})'
-    ExportDialog_size: ``,  // TODO: 'Size'
-    ExportDialog_size_S: ``,  // TODO: 'small ({0})'
-    ExportDialog_size_M: ``,  // TODO: 'medium ({0})'
-    ExportDialog_size_L: ``,  // TODO: 'large ({0})'
-    ExportDialog_size_original: ``,  // TODO: 'original size'
-    ExportDialog_size_custom: ``,  // TODO: 'custom'
-    ExportDialog_customSizeSettings: ``,  // TODO: 'Maximum {0} of {1} pixels'
-    ExportDialog_customSizeSide_width: ``,  // TODO: 'width'
-    ExportDialog_customSizeSide_height: ``,  // TODO: 'height'
-    ExportDialog_customSizeSide_size: ``,  // TODO: 'size'
-    ExportDialog_privacy: ``,  // TODO: 'Privacy'
-    ExportDialog_removeInfo: ``,  // TODO: 'Remove extra information'
-    ExportDialog_removeInfo_desc: ``,  // TODO: 'When selected, extra information like EXIF-Tags will be removed from the exported photo. This is information about the camera used, the location where the photo was taken or similar.'
-    ExportDialog_fileName: ``,  // TODO: 'File name'
-    ExportDialog_fileName_likeOriginal: ``,  // TODO: 'Like original'
-    ExportDialog_fileName_sequence: ``,  // TODO: 'sequence'
-    ExportDialog_fileName_sequencePrefix: ``,  // TODO: 'Prefix'
-    ExportDialog_fileName_sequencePrefixDefault: ``,  // TODO: 'Photo'
-    ExportDialog_fileName_sequenceExample: ``,  // TODO: 'Example: {0}'
-    ExportDialog_export: ``,  // TODO: 'Export'
-    ExportDialog_exportingTo: ``,  // TODO: 'Exporting to {0}'
-    ExportDialog_done_one: ``,  // TODO: 'Finished exporting photo'
-    ExportDialog_done_more: ``,  // TODO: 'Finished exporting {0} photos'
+    ImportProgressButton_phase_scan_dirs: "Escaneando carpetas",
+    ImportProgressButton_phase_cleanup: "Limpiando",
+    ImportProgressButton_phase_import_photos: "Importando",
+    ImportProgressButton_phase_error: "Escaneo fallido",
+    ImportProgressButton_resume: "Reanudar",
+    ImportProgressButton_pause: "Pausa",
 
-    ImportProgressButton_phase_scan_dirs: 'Escaneando carpetas',
-    ImportProgressButton_phase_cleanup: 'Limpiando',
-    ImportProgressButton_phase_import_photos: 'Importando',
-    ImportProgressButton_phase_error: 'Escaneo fallido',
-    ImportProgressButton_resume: 'Reanudar',
-    ImportProgressButton_pause: 'Pausa',
+    ImportController_importFinished: "Escaneadas {0} fotos en {1}",
 
-    ImportController_importFinished: 'Escaneadas {0} fotos en {1}',
-
-    Library_noPhotos_title: 'No hay fotos añadidas',
-    Library_noSettings_message: 'Haz click en el {0} arriba para abrir los ajustes.',
-    Library_noPhotos_message: 'Presiona {0} o el botón más abajo para empezar el escaneo de las fotos en las carpetas.',
-    Library_startScanning: 'Comenzar el escaneo',
-    Library_emptyTrash: 'La papelera está vacía',
+    Library_noPhotos_title: "No hay fotos añadidas",
+    Library_noSettings_message:
+        "Haz click en el {0} arriba para abrir los ajustes.",
+    Library_noPhotos_message:
+        "Presiona {0} o el botón más abajo para empezar el escaneo de las fotos en las carpetas.",
+    Library_startScanning: "Comenzar el escaneo",
+    Library_emptyTrash: "La papelera está vacía",
     Library_emptyFavorites: "No mascaste ninguna foto como favorita todavía",
     Library_emptyView: "No hay ninguna foto en tu vista actual",
-    Library_selectOtherView: 'Por favor, selecciona otra vista en la parte superior a la izquierda.',
+    Library_selectOtherView:
+        "Por favor, selecciona otra vista en la parte superior a la izquierda.",
 
-    LibraryBottomBar_selected: '{0} selectionados',
-    LibraryBottomBar_deselect: 'Deseleccionar',
-    LibraryBottomBar_photoCount_one: '1 foto',
-    LibraryBottomBar_photoCount_more: '{0} fotos',
+    LibraryBottomBar_selected: "{0} selectionados",
+    LibraryBottomBar_deselect: "Deseleccionar",
+    LibraryBottomBar_photoCount_one: "1 foto",
+    LibraryBottomBar_photoCount_more: "{0} fotos",
 
-    LibraryFilterButton_shown: 'Mostrado: {0}',
-    LibraryFilterButton_filter_all: 'Todoas las fotos',
-    LibraryFilterButton_filter_flagged: 'Favoritos',
-    LibraryFilterButton_filter_processed: 'Editado',
-    LibraryFilterButton_filter_trash: 'Papelera',
+    LibraryFilterButton_shown: "Mostrado: {0}",
+    LibraryFilterButton_filter_all: "Todoas las fotos",
+    LibraryFilterButton_filter_flagged: "Favoritos",
+    LibraryFilterButton_filter_processed: "Editado",
+    LibraryFilterButton_filter_trash: "Papelera",
     LibraryFilterButton_filter_tag: 'Con etiqueta "{0}"',
-    LibraryFilterButton_tag_title: 'Con etiqueta:',
+    LibraryFilterButton_tag_title: "Con etiqueta:",
 
-    LibraryTopBar_emptyTrashQuestion: 'Estás seguro de que quieres vaciar la papelera?',
-    LibraryTopBar_moveToTrash: 'Mover a la papelera del sistema operativo',
-    LibraryTopBar_emptyTrash: 'Vaciar papelera',
+    LibraryTopBar_emptyTrashQuestion:
+        "Estás seguro de que quieres vaciar la papelera?",
+    LibraryTopBar_moveToTrash: "Mover a la papelera del sistema operativo",
+    LibraryTopBar_emptyTrash: "Vaciar papelera",
 
-    MainMenu_version: ``,  // TODO: 'Version {0}'
-    MainMenu_settings: 'Ajustes',
-    MainMenu_quit: ``,  // TODO: 'Quit'
-    MainMenu_file: ``,  // TODO: 'File'
-    MainMenu_export: ``,  // TODO: 'Export photos'
+    MainMenu_version: `Version {0}`,
+    MainMenu_settings: "Ajustes",
+    MainMenu_quit: `Salir`,
+    MainMenu_file: `Archivo`,
+    MainMenu_export: `Exportar fotos`,
     MainMenu_scan: `Scanear en busca de imágenes`,
-    MainMenu_view: ``,  // TODO: 'View'
-    MainMenu_toggleFullScreen: ``,  // TODO: 'Toggle Fullscreen'
-    MainMenu_developer: ``,  // TODO: 'Developer'
-    MainMenu_toggleDevTools: ``,  // TODO: 'Toggle development tools'
-    MainMenu_toggleUiTester: ``,  // TODO: 'Toggle UI Tester'
-    MainMenu_reloadUi: ``,  // TODO: 'Reload UI'
+    MainMenu_view: `Ver`,
+    MainMenu_toggleFullScreen: `Mostrar pantalla completa`,
+    MainMenu_developer: `Desarrollador`,
+    MainMenu_toggleDevTools: `Mostrar herramientas de desarrollador`,
+    MainMenu_toggleUiTester: `Mostrar probador de UI`,
+    MainMenu_reloadUi: `Recargar UI`,
 
-    Picture_error_createThumbnail: ``,  // TODO: 'Creating thumbnail failed'
+    Picture_error_createThumbnail: `Falló la creación de la miniatura`,
 
-    PhotoActionButtons_movedToTrash_one: 'Mover foto a la papelera',
-    PhotoActionButtons_movedToTrash_more: 'Movidas {0} fotos a la papelera',
-    PhotoActionButtons_restoredFromTrash_one: 'Foto restaurada de la papelera',
-    PhotoActionButtons_restoredFromTrash_more: 'Restauradas {0} fotos de la papelera',
-    PhotoActionButtons_removeFavorite: 'Elminar de favoritos',
-    PhotoActionButtons_addFavorite: 'Añadir a favoritos',
-    PhotoActionButtons_trash: 'Mover a la papelera',
-    PhotoActionButtons_restoreFromTrash: 'Restaurar de la papelera',
-    PhotoActionButtons_restore: 'Restaurar',
-    PhotoActionButtons_photoInfo: 'Mostrar información sobre la foto seleccionada',
-    PhotoActionButtons_export: 'Exportar',
+    PhotoActionButtons_movedToTrash_one: "Mover foto a la papelera",
+    PhotoActionButtons_movedToTrash_more: "Movidas {0} fotos a la papelera",
+    PhotoActionButtons_restoredFromTrash_one: "Foto restaurada de la papelera",
+    PhotoActionButtons_restoredFromTrash_more:
+        "Restauradas {0} fotos de la papelera",
+    PhotoActionButtons_removeFavorite: "Elminar de favoritos",
+    PhotoActionButtons_addFavorite: "Añadir a favoritos",
+    PhotoActionButtons_trash: "Mover a la papelera",
+    PhotoActionButtons_restoreFromTrash: "Restaurar de la papelera",
+    PhotoActionButtons_restore: "Restaurar",
+    PhotoActionButtons_photoInfo:
+        "Mostrar información sobre la foto seleccionada",
+    PhotoActionButtons_export: "Exportar",
 
-    PhotoInfo_title: 'Info',
-    PhotoInfo_showInFolder: 'Mostrar foto en carpeta',
-    PhotoInfo_copyPath: 'Copiar ruta',
-    PhotoInfo_copyCoordinates: ``,  // TODO: 'Copy coordinates'
-    PhotoInfo_noSelection_title: 'No hay fotos seleccionadas',
-    PhotoInfo_noSelection_message: 'Por favor, selecciona una foto de la izquierda.',
-    PhotoInfo_exifData: ``,  // TODO: 'Exif data'
-    PhotoInfo_show: ``,  // TODO: 'show'
-    PhotoInfo_hide: ``,  // TODO: 'hide'
-    PhotoInfo_showAll: ``,  // TODO: 'show all'
-    PhotoInfo_showLess: ``,  // TODO: 'show less'
-    PhotoInfo_andMore: ``,  // TODO: 'and {0} more'
-    PhotoInfo_exifTitle_ifd0: ``,  // TODO: 'Image (IFD0)'
-    PhotoInfo_exifTitle_ifd1: ``,  // TODO: 'Thumbnail (IFD1)'
-    PhotoInfo_noValue: ``,  // TODO: 'Photo has no {0}'
-    PhotoInfo_error_fetchPhotoSize: ``,  // TODO: 'Getting photo size failed'
+    PhotoInfo_title: "Info",
+    PhotoInfo_showInFolder: "Mostrar foto en carpeta",
+    PhotoInfo_copyPath: "Copiar ruta",
+    PhotoInfo_copyCoordinates: `Copiar coordenadas`,
+    PhotoInfo_noSelection_title: "No hay fotos seleccionadas",
+    PhotoInfo_noSelection_message:
+        "Por favor, selecciona una foto de la izquierda.",
+    PhotoInfo_exifData: `Datos Exif`,
+    PhotoInfo_show: `mostrar`,
+    PhotoInfo_hide: `ocultar`,
+    PhotoInfo_showAll: `mostrar todo`,
+    PhotoInfo_showLess: `mostrar menos`,
+    PhotoInfo_andMore: `y {0} más`,
+    PhotoInfo_exifTitle_ifd0: `Imagen (IFD0)`,
+    PhotoInfo_exifTitle_ifd1: `Miniatura (IFD1)`,
+    PhotoInfo_noValue: `La foto no tiene {0}`,
+    PhotoInfo_error_fetchPhotoSize: `No se pudo obtener el tamaño de la foto`,
 
-    PhotoDetailPane_prevPhoto: 'Anterior foto',
-    PhotoDetailPane_nextPhoto: 'Siguiente foto',
-    PhotoDetailPane_edit: 'Editar',
+    PhotoDetailPane_prevPhoto: "Anterior foto",
+    PhotoDetailPane_nextPhoto: "Siguiente foto",
+    PhotoDetailPane_edit: "Editar",
 
-    RotateButtonGroup_rotateLeft: 'Rotar a la izquierda',
-    RotateButtonGroup_rotateRight: 'Rotar a la derecha',
+    RotateButtonGroup_rotateLeft: "Rotar a la izquierda",
+    RotateButtonGroup_rotateRight: "Rotar a la derecha",
 
-    Settings_title: 'Ajustes',
-    Settings_selectPhotoDirs: 'Por favor, selecciona carpetas para escanear imágenes.',
-    Settings_photoDirs: 'Carpetas para escanear imágenes:',
-    Settings_addPhotoDir: 'Añadir carpeta de fotos',
-    Settings_startScan: 'Scanear en busca de imágenes',
-
-    TagEditor_addTag: 'Añadir etiqueta...',
-    TagEditor_error_tagExists: 'Por favor, introduce una nueva etiqueta',
-
-}
+    Settings_title: "Ajustes",
+    Settings_selectPhotoDirs:
+        "Por favor, selecciona carpetas para escanear imágenes.",
+    Settings_photoDirs: "Carpetas para escanear imágenes:",
+    Settings_addPhotoDir: "Añadir carpeta de fotos",
+    Settings_startScan: "Scanear en busca de imágenes",
+    TagEditor_addTag: "Añadir etiqueta...",
+    TagEditor_error_tagExists: "Por favor, introduce una nueva etiqueta"
+};
